fac92f8c677fb1dc84870b931f19d62e19123c73
yangdp1983 <741744094@qq.com>
2020-09-06 07:12:11 +0800

    first commit

---
+++ src/main/java/com/redis/demo/RedisActionApplication.java
@@ -0,0 +1,13 @@
+package com.redis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redis/demo/RedisActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
